c43bdeb808a28237fa148d64352e58ebc8d0c586
sakon-ziwave <ryo.development0320@gmail.com>
2022-04-01 20:31:07 +0900

    縦20、横10のマスを定義

--- src/pages/PlayPage.vue
+++ src/pages/PlayPage.vue
@@ -1,4 +1,13 @@
 <script setup lang="ts">
+const row = 20;
+const field = new Array(row);
+
+const column = 10;
+for (let i = 0; i < row; i++) {
+    const fieldColumn = new Array(column).fill(0);
+    field[i] = fieldColumn[i];
+}
+
 </script>
 
 <template>
